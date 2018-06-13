bf3744b13e805b447ae91867fe386d7ff927de12
Евгений Самсонов <samsonov_eugene@mail.ru>
2018-06-13 23:54:32 +0300

    Исправление ошибок, повышение производительности

--- app/src/main/java/ru/shadowsparky/autisticsdevelopers/poliklinika/AddAppointmentMenu.java
+++ app/src/main/java/ru/shadowsparky/autisticsdevelopers/poliklinika/AddAppointmentMenu.java
@@ -2,13 +2,39 @@ package ru.shadowsparky.autisticsdevelopers.poliklinika;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AddAppointmentMenu extends AppCompatActivity {
-
+    String []data = {"test", "test"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_appointment_menu);
         getSupportActionBar().setTitle("Добавление записи");
+        FillAvailableDocs();
+    }
+
+    private void FillAvailableDocs(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+        spinner.setPrompt("Выберите специальность");
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), "test - " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
