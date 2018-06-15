8020a017f77550395640f9b3417d44d4f21664e3
Евгений Самсонов <samsonov_eugene@mail.ru>
2018-06-15 20:33:48 +0300

    Исправление ошибок, повышение производительности
    Исправление ошибок, повышение производительности

--- app/src/main/java/ru/shadowsparky/autisticsdevelopers/poliklinika/AddAppointmentMenu.java
+++ app/src/main/java/ru/shadowsparky/autisticsdevelopers/poliklinika/AddAppointmentMenu.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -16,6 +17,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
 
 public class AddAppointmentMenu extends AppCompatActivity {
@@ -26,7 +28,7 @@ public class AddAppointmentMenu extends AppCompatActivity {
     private Spinner DocSpinner;
     private String choosedDate;
     private String currentDocNumber;
-    TextView tv;
+    Spinner DateSpinner;
     DatePickerDialog DPD;
     String[] docNumbers;
     String[] docsData;
@@ -35,19 +37,18 @@ public class AddAppointmentMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_appointment_menu);
+        DateSpinner = findViewById(R.id.DateSpinner);
+        DocSpinner = findViewById(R.id.DoctorsSpinner);
+        DateSpinner.setEnabled(false);
+        DocSpinner.setEnabled(false);
+        FillAvailableSpec(catchSpec());
         getSupportActionBar().setTitle("Запись на прием");
         final Calendar cal = Calendar.getInstance(TimeZone.getDefault());
         year_x = cal.get(Calendar.YEAR);
         month_x = cal.get(Calendar.MONTH);
         day_x = cal.get(Calendar.DAY_OF_MONTH);
-        FillAvailableSpec(catchSpec());
-        tv = findViewById(R.id.dateView);
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createDialog();
-            }
-        });
+
+        DateSpinner.setOnTouchListener(spinnerOnTouch);
     }
     private String[] catchSpec(){
         String[] result = null;
@@ -73,7 +74,13 @@ public class AddAppointmentMenu extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Выбор специализации
+                clearSpinner(DateSpinner);
+                clearSpinner(DocSpinner);
                 selectedSpecID = position;
+                DateSpinner.setEnabled(true);
+                String[] txt = {"Выберите дату"};
+                changeDateSpinner(txt);
             }
 
             @Override
@@ -82,50 +89,81 @@ public class AddAppointmentMenu extends AppCompatActivity {
             }
         });
     }
+    private View.OnTouchListener spinnerOnTouch = new View.OnTouchListener() {
+        public boolean onTouch(View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                DPD = new DatePickerDialog(AddAppointmentMenu.this, dpickerListner, year_x, month_x, day_x);
+                DPD.show();
+            }
+            return false;
+        }
+    };
 
-    private void createDialog(){
-        DPD = new DatePickerDialog(this, dpickerListner, year_x, month_x, day_x);
-        DPD.setTitle("Выберите дату");
-        DPD.show();
+    private void changeDateSpinner(String[] res){
+        customSpinner cs = new customSpinner();
+        ArrayAdapter<String> adapter = customSpinner.throwCustomSpinner(this, android.R.layout.simple_spinner_item, res);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        DateSpinner.setAdapter(adapter);
+        clearSpinner(DocSpinner);
+        DocSpinner.setEnabled(true);
     }
+
+    private void clearSpinner(Spinner _spinner){
+        String [] emptyArray = {""};
+        ArrayAdapter<String> adapter = customSpinner.throwCustomSpinner(this, android.R.layout.simple_spinner_item, emptyArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        _spinner.setAdapter(adapter);
+    }
+
     private DatePickerDialog.OnDateSetListener dpickerListner = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
             year_x = year;
-            month_x = month + 1;
+            month_x = month;
+            int FixedMonth = month + 1;
             day_x = dayOfMonth;
-            choosedDate = day_x+ "."+ month_x + "."+year_x;
-            tv.setText(choosedDate);
+            choosedDate = day_x+ "."+ FixedMonth + "."+year_x;
+            String[] Date = {choosedDate};
+            changeDateSpinner(Date);
             setDoctors();
         }
     };
 
     private void setDoctors() {
         String[] bindValues = {"Key", "Position", "Date"};
+        String[] error = {"Нет доступных врачей"};
         String[] Values = {"EnableExecute", spinner.getSelectedItem().toString(), choosedDate};
         SQL_AvailableDoctors SA = new SQL_AvailableDoctors(bindValues, Values, "https://autisticapi.shadowsparky.ru/getavailabledocs.php");
         ArrayList<SQL_Engine> res = SA.CatchResult();
-        this.docNumbers = new String[res.size()];
-        this.docsData = new String[res.size()];
+        this.docNumbers = new String[res.size() + 1];
+        this.docsData = new String[res.size() + 1];
+        this.docsData[0] = "Выберите врача";
         for (int i = 0; i < res.size(); i++){
             if (((SQL_AvailableDoctors)res.get(i)).get_Result() == null) {
-                docsData[i] =  (((SQL_AvailableDoctors)res.get(i)).getFirst_Name() + " " + ((SQL_AvailableDoctors)res.get(i)).getLast_Name() + " " + ((SQL_AvailableDoctors)res.get(i)).getPathronymic());
-                docNumbers[i] = ((SQL_AvailableDoctors)res.get(i)).getDoctor_Number();
+                String TMPStr = ((SQL_AvailableDoctors)res.get(i)).getFirst_Name() + " " + ((SQL_AvailableDoctors)res.get(i)).getLast_Name() + " " + ((SQL_AvailableDoctors)res.get(i)).getPathronymic();
+                if (!TMPStr.equals("null null null")) {
+                    docsData[i+1] = (TMPStr);
+                    docNumbers[i+1] = ((SQL_AvailableDoctors) res.get(i)).getDoctor_Number();
+                }
             }
+        }
+        if (docsData[1] == null) {
+            this.docsData = error;
+            Toast.makeText(this, R.string.DoctorsSelectFail, Toast.LENGTH_SHORT).show();
         }
         fillAvaliableDocs();
     }
 
     private void fillAvaliableDocs(){
         DocSpinner = findViewById(R.id.DoctorsSpinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, docsData);
+        ArrayAdapter<String> adapter = customSpinner.throwCustomSpinner(this, android.R.layout.simple_spinner_item, docsData);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        DocSpinner = findViewById(R.id.DoctorsSpinner);
         DocSpinner.setAdapter(adapter);
         DocSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(AddAppointmentMenu.this, docNumbers[position] + " " + DocSpinner.getSelectedItem().toString(), Toast.LENGTH_SHORT);
+                // Выбор врача
+                Toast.makeText(AddAppointmentMenu.this, docNumbers[position] + " " + DocSpinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
