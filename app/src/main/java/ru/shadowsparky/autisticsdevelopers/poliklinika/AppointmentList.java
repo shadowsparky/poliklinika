61bc658a636ea89d39473eecc37b3e7b8c824377
Eugene <samsonov_eugene@mail.ru>
2018-06-25 19:01:09 +0300

    тест

--- app/src/main/java/ru/shadowsparky/autisticsdevelopers/poliklinika/AppointmentList.java
+++ app/src/main/java/ru/shadowsparky/autisticsdevelopers/poliklinika/AppointmentList.java
@@ -29,45 +29,46 @@ public class AppointmentList extends ListFragment {
     }
 
     private void setAppointments() {
-        ArrayList<HashMap<String, String>> data = throwListResult();
-        if (data != null) {
-            SimpleAdapter adapter = new SimpleAdapter(getActivity(), data, android.R.layout.simple_list_item_2,
-                    new String[] {"Specialization", "Date and Time"},
-                    new int[]{android.R.id.text1, android.R.id.text2});
-            setListAdapter(adapter);
-        }
+//        ArrayList<HashMap<String, String>> data = throwListResult();
+//        if (data != null) {
+//            SimpleAdapter adapter = new SimpleAdapter(getActivity(), data, android.R.layout.simple_list_item_2,
+//                    new String[] {"Specialization", "Date and Time"},
+//                    new int[]{android.R.id.text1, android.R.id.text2});
+//            setListAdapter(adapter);
+//        }
     }
 
     private ArrayList<HashMap<String, String>> throwListResult(){
-        ArrayList<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
-
-        tv = (TextView) getActivity().findViewById(R.id.isEmptyTV);
-        String [] bindValues = {"Key", "Login"};
-        String [] values = {"EnableExecute", Auth_Menu.getLogin()};
-        if (getAppointmentList(result, bindValues, values)) return null;
-        return result;
+//        ArrayList<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
+//
+//        tv = (TextView) getActivity().findViewById(R.id.isEmptyTV);
+//        String [] bindValues = {"Key", "Login"};
+//        String [] values = {"EnableExecute", Auth_Menu.getLogin()};
+//        if (getAppointmentList(result, bindValues, values)) return null;
+//        return result;
+        return null;
     }
 
     private boolean getAppointmentList(ArrayList<HashMap<String, String>> result, String[] bindValues, String[] values) {
-        SQL_GetUserAppointments SG = new SQL_GetUserAppointments(bindValues, values, "https://autisticapi.shadowsparky.ru/getalluserappointments.php");
-        SG.set_context(getActivity());
-        ArrayList<SQL_Engine> res = SG.CatchResult();
-        ids = new String[res.size()];
-        if (!((SQL_GetUserAppointments)res.get(0)).getAppointmentNumber().equals("Записи отсутствуют")) {
-            for (int i = 0; i < res.size(); i++) {
-                ids[i] = ((SQL_GetUserAppointments) res.get(i)).getAppointmentNumber();
-                HashMap<String, String> TMPResult = new HashMap<String, String>();
-                TMPResult.put("Specialization",((SQL_GetUserAppointments) res.get(i)).getPosition());
-                TMPResult.put("Date and Time", ( "Время: "+((SQL_GetUserAppointments) res.get(i)).getTime() + ", дата: " + ((SQL_GetUserAppointments) res.get(i)).getDate()));
-                result.add(TMPResult);
-            }
-            tv.setVisibility(View.INVISIBLE);
-            getView().setVisibility(View.VISIBLE);
-        } else {
-            getView().setVisibility(View.INVISIBLE);
-            tv.setVisibility(View.VISIBLE);
-            return true;
-        }
+//        SQL_GetUserAppointments SG = new SQL_GetUserAppointments(bindValues, values, "https://autisticapi.shadowsparky.ru/getalluserappointments.php");
+//        SG.set_context(getActivity());
+//        ArrayList<SQL_Engine> res = SG.CatchResult();
+//        ids = new String[res.size()];
+//        if (!((SQL_GetUserAppointments)res.get(0)).getAppointmentNumber().equals("Записи отсутствуют")) {
+//            for (int i = 0; i < res.size(); i++) {
+//                ids[i] = ((SQL_GetUserAppointments) res.get(i)).getAppointmentNumber();
+//                HashMap<String, String> TMPResult = new HashMap<String, String>();
+//                TMPResult.put("Specialization",((SQL_GetUserAppointments) res.get(i)).getPosition());
+//                TMPResult.put("Date and Time", ( "Время: "+((SQL_GetUserAppointments) res.get(i)).getTime() + ", дата: " + ((SQL_GetUserAppointments) res.get(i)).getDate()));
+//                result.add(TMPResult);
+//            }
+//            tv.setVisibility(View.INVISIBLE);
+//            getView().setVisibility(View.VISIBLE);
+//        } else {
+//            getView().setVisibility(View.INVISIBLE);
+//            tv.setVisibility(View.VISIBLE);
+//            return true;
+//        }
         return false;
     }
 
@@ -85,32 +86,32 @@ public class AppointmentList extends ListFragment {
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        String[] bindValues = {"Key", "AppointmentNumber"};
-        String[] values = {"EnableExecute", ids[position]};
-        getAppointmentInfo(position, bindValues, values);
+//        String[] bindValues = {"Key", "AppointmentNumber"};
+//        String[] values = {"EnableExecute", ids[position]};
+//        getAppointmentInfo(position, bindValues, values);
     }
 
     private void getAppointmentInfo(int position, String[] bindValues, String[] values) {
-        SQL_GetAdditionalAppointmentInfo SGAAI = new SQL_GetAdditionalAppointmentInfo(bindValues, values, "https://autisticapi.shadowsparky.ru/getAdditionalAppointmentInfo.php");
-        ArrayList<SQL_Engine> res = SGAAI.CatchResult();
-        if (res != null) {
-            if (res.size() != 0) {
-                String [] TMPResultArray = {((SQL_GetAdditionalAppointmentInfo) res.get(0)).getPacientFirstName() + " " + ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getPacientLastName() + " " + ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getPacientPathronymic(),
-                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getDoctorFirstName() + " " + ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getDoctorLastName() + " " +
-                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getDoctorPathronymic(),
-                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getPosition(),
-                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getDate(),
-                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getTime(),
-                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getCabinetNumber()
-                };
-                Intent i = new Intent(getActivity(), DeployedAppointmentActivity.class);
-                String[] valuesIntent = {"PacientInfo", "DoctorInfo", "Position", "Date", "Time", "CabinetNumber"};
-                putin(i, valuesIntent, TMPResultArray);
-                i.putExtra("AppointmentNumber", ids[position]);
-                startActivity(i);
-            } else {
-                Toast.makeText(getActivity(), "Запись не найдена", Toast.LENGTH_SHORT).show();}
-        } else {Toast.makeText(getActivity(), getResources().getString(R.string.connection_error), Toast.LENGTH_SHORT).show();}
+//        SQL_GetAdditionalAppointmentInfo SGAAI = new SQL_GetAdditionalAppointmentInfo(bindValues, values, "https://autisticapi.shadowsparky.ru/getAdditionalAppointmentInfo.php");
+//        ArrayList<SQL_Engine> res = SGAAI.CatchResult();
+//        if (res != null) {
+//            if (res.size() != 0) {
+//                String [] TMPResultArray = {((SQL_GetAdditionalAppointmentInfo) res.get(0)).getPacientFirstName() + " " + ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getPacientLastName() + " " + ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getPacientPathronymic(),
+//                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getDoctorFirstName() + " " + ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getDoctorLastName() + " " +
+//                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getDoctorPathronymic(),
+//                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getPosition(),
+//                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getDate(),
+//                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getTime(),
+//                        ((SQL_GetAdditionalAppointmentInfo) res.get(0)).getCabinetNumber()
+//                };
+//                Intent i = new Intent(getActivity(), DeployedAppointmentActivity.class);
+//                String[] valuesIntent = {"PacientInfo", "DoctorInfo", "Position", "Date", "Time", "CabinetNumber"};
+//                putin(i, valuesIntent, TMPResultArray);
+//                i.putExtra("AppointmentNumber", ids[position]);
+//                startActivity(i);
+//            } else {
+//                Toast.makeText(getActivity(), "Запись не найдена", Toast.LENGTH_SHORT).show();}
+//        } else {Toast.makeText(getActivity(), getResources().getString(R.string.connection_error), Toast.LENGTH_SHORT).show();}
     }
 
     private void putin(Intent i, String[] values, String...args){
--- app/src/main/java/ru/shadowsparky/autisticsdevelopers/poliklinika/UserMenu.java
+++ app/src/main/java/ru/shadowsparky/autisticsdevelopers/poliklinika/UserMenu.java
@@ -2,20 +2,48 @@ package ru.shadowsparky.autisticsdevelopers.poliklinika;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 public class UserMenu extends AppCompatActivity implements View.OnClickListener {
-
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_menu);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
         getSupportActionBar().setTitle(R.string.My_Appointments);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
     }
 
     @Override
@@ -42,5 +70,14 @@ public class UserMenu extends AppCompatActivity implements View.OnClickListener 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.exitmenu, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
